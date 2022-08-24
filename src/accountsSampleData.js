e450d1886eb3aa4aee5d5fd5c6d3e6e0cda94dbc
dantaayy <dantaayy@gmail.com>
2022-08-24 09:25:14 -0700

    added Schema, and data

---
+++ src/accountsSampleData.js
@@ -0,0 +1,154 @@
+let accounts = [
+    {
+      "_id": "63064a759c089d8cf072494d",
+      "isActive": true,
+      "balance": "$1,521.90",
+      "name": "Kim Floyd",
+      "email": "kimfloyd@pasturia.com",
+      "phone": "+1 (817) 585-2149",
+      "address": "553 Fane Court, Bergoo, Ohio, 4290",
+      "openDate": "2019-10-26T11:12:52 +07:00",
+      "purchases": [
+        {
+          "product": "Yeezy 350V2 Boost",
+          "price": "$378.85"
+        },
+        {
+          "product": "Lululemon Jacket",
+          "price": "$84.23"
+        },
+        {
+          "product": "HyperX 16GB Ram",
+          "price": "$246.10"
+        }
+      ],
+      "greeting": "Hello, Kim Floyd! You have 2 unread messages."
+    },
+    {
+      "_id": "63064a7507295860141cd2c4",
+      "isActive": true,
+      "balance": "$4,294.95",
+      "name": "Pollard Mcgowan",
+      "email": "pollardmcgowan@pasturia.com",
+      "phone": "+1 (904) 579-2789",
+      "address": "851 Brevoort Place, Kennedyville, Washington, 3229",
+      "openDate": "2022-08-01T08:43:55 +07:00",
+      "purchases": [
+        {
+          "product": "Nespresso",
+          "price": "$322.86"
+        },
+        {
+          "product": "Amazon Echo",
+          "price": "$71.77"
+        },
+        {
+          "product": "Standing Desk Ikea",
+          "price": "$483.57"
+        }
+      ],
+      "greeting": "Hello, Pollard Mcgowan! You have 3 unread messages."
+    },
+    {
+      "_id": "63064a759761f22f4bbd7fc8",
+      "isActive": true,
+      "balance": "$3,812.34",
+      "name": "Barber Hickman",
+      "email": "barberhickman@pasturia.com",
+      "phone": "+1 (928) 459-3560",
+      "address": "343 Bridgewater Street, Caberfae, District Of Columbia, 9416",
+      "openDate": "2018-08-21T10:02:13 +07:00",
+      "purchases": [
+        {
+          "product": "LG 34 in Ultrawide Screen",
+          "price": "$462.68"
+        },
+        {
+          "product": "SONY Overear Headphones",
+          "price": "$361.83"
+        },
+        {
+          "product": "Ergonomic Chair",
+          "price": "$406.40"
+        }
+      ],
+      "greeting": "Hello, Barber Hickman! You have 4 unread messages."
+    },
+    {
+      "_id": "63064a75d43afc0b021a2adc",
+      "isActive": false,
+      "balance": "$3,677.38",
+      "name": "Cherry Lara",
+      "email": "cherrylara@pasturia.com",
+      "phone": "+1 (889) 447-3372",
+      "address": "587 Hausman Street, Ruckersville, Federated States Of Micronesia, 3495",
+      "openDate": "2016-06-04T11:38:39 +07:00",
+      "purchases": [
+        {
+          "product": "Wifi Router",
+          "price": "$240.10"
+        },
+        {
+          "product": "Costco bed Comforter",
+          "price": "$357.73"
+        },
+        {
+          "product": "Ikea drawer set",
+          "price": "$309.41"
+        }
+      ],
+      "greeting": "Hello, Cherry Lara! You have 1 unread messages."
+    },
+    {
+      "_id": "63064a753e408ca8608c9a0b",
+      "isActive": false,
+      "balance": "$4,463.76",
+      "name": "Kim Combs",
+      "email": "kimcombs@pasturia.com",
+      "phone": "+1 (842) 540-3734",
+      "address": "546 Voorhies Avenue, Madaket, Alaska, 3563",
+      "openDate": "2018-07-10T10:49:51 +07:00",
+      "purchases": [
+        {
+          "product": "Dumbell set",
+          "price": "$359.19"
+        },
+        {
+          "product": "Beats wireless headphones",
+          "price": "$111.06"
+        },
+        {
+          "product": "Oil change and new tires for car",
+          "price": "$499.54"
+        }
+      ],
+      "greeting": "Hello, Kim Combs! You have 8 unread messages."
+    },
+    {
+      "_id": "63064a755b892316b1916525",
+      "isActive": false,
+      "balance": "$1,679.86",
+      "name": "Marion Gordon",
+      "email": "mariongordon@pasturia.com",
+      "phone": "+1 (948) 554-3659",
+      "address": "614 Scholes Street, Ebro, Hawaii, 5152",
+      "openDate": "2019-10-08T08:37:33 +07:00",
+      "purchases": [
+        {
+          "product": "Nike Air Jordan 1 Bred",
+          "price": "$168.01"
+        },
+        {
+          "product": "Costco mattress",
+          "price": "$446.30"
+        },
+        {
+          "product": "Ikea couhc",
+          "price": "$351.95"
+        }
+      ],
+      "greeting": "Hello, Marion Gordon! You have 5 unread messages."
+    }
+]
+
+module.exports = accounts;
